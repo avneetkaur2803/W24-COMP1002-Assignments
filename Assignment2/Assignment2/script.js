fea513a7897b66ca2329df0f65a7c43cf9e941d3
avneetkaur2803 <156447358+avneetkaur2803@users.noreply.github.com>
2024-08-08 21:18:47 -0400

    Delete Assignment2/Assignment2 directory

--- Assignment2/Assignment2/script.js
+++
@@ -1,9 +0,0 @@
-// fonction to display navigation bar's links when hamburger icons are hovered over
-document.getElementById('burger-icon').addEventListener('click', function () {
-    var navigationBar = document.getElementById('navigationBar');
-    if (getComputedStyle(navigationBar).display === 'none') {
-        navigationBar.style.display = 'block';
-    } else {
-        navigationBar.style.display = 'none';
-    }
-});
